f80d825af094e07e7611fa82fdf58e21880760fb
Naiym Harris <nharris@naimac.local>
2023-03-12 18:59:43 -0400

    added get_product function

--- accounts.py
+++ accounts.py
@@ -5,11 +5,12 @@ con = sqlite3.connect("smart_register.sqlite")
 cur = con.cursor()
 
 
-def display_accounts():
+def display_accounts():  # display all accounts
     query = 'select * from accounts'
     print(pd.read_sql_query(query, con))
 
 
+# update account balances given given account rfid number and amount
 def update_balance(rfid, amount):
     query = f"select balance from accounts where rfid='{rfid}'"
     balance = cur.execute(query).fetchone()[0]
@@ -18,11 +19,18 @@ def update_balance(rfid, amount):
     con.commit()
 
 
+def get_product(upc):  # returns name and price of product given upc
+    query = f"select item_name, item_cost from catalog where item_upc='{upc}'"
+    name, price = cur.execute(query).fetchone()  # [0]
+    cur.execute(query)
+    con.commit()
+    return name, price
+
+
 def main():
-    rfid = '1002000100'
-    display_accounts()
-    update_balance(rfid, 200000)
-    display_accounts()
+    upc = '978215396047'
+    name, price = get_product(upc)
+    print(name, price)
 
 
 if __name__ == '__main__':
